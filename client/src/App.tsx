4615828c703c66bf3a28b500438ff955c5931fb6
Jo YoHan <whdygks210@naver.com>
2022-05-11 20:30:31 +0900

    client
    - memo model 추가
    - 메모 추가 UI 구현 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Grid, TextField, Button } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  let [title, setTitle] = useState("");
+  let [content, setContent] = useState("");
+
+  function submit() {
+    console.log(title + "  " + content);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={2} style={{ height:"auto" }}>
+        <Grid item style={{ textAlign: "center", width: "100%" }}>
+          <TextField onChange={(e) => { setTitle(e.target.value) }} value={title} label="Title" style={{ width:"500px" }} />
+        </Grid>
+        <Grid item style={{ textAlign: "center", width: "100%", height:"200px" }}>
+          <TextField onChange={(e) => { setContent(e.target.value) }} value={content} id="outlined-multiline-static" label="Content" multiline rows={6} style={{ width:"500px" }} />
+        </Grid>
+        <Grid item style={{ textAlign: "center", width: "100%" }}>
+          <Button onClick={submit} variant="contained">Commit</Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ client/src/model/Memo.ts
@@ -0,0 +1,7 @@
+type Memo = {
+  id:       number;
+  title:    string;
+  content:  string;
+};
+
+export type { Memo };
